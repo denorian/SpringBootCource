bb3ed7371fddc0c7bea0065344affa972e7e2dba
denorian <denorian@yandex.ru>
2020-01-28 19:11:20 +0300

    fix test

--- src/test/java/com/example/sweater/service/UserServiceTest.java
+++ src/test/java/com/example/sweater/service/UserServiceTest.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class UserServiceTest {
 	
 	@Autowired
